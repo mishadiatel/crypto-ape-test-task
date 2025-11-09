5e12a69f89403d7e28b9e34ed6cdb16c02ae010a
Mykhailo Diatel <mishadiatel13@gmail.com>
2025-11-09 16:07:59 +0200

    moved api url to .env file

--- api/crypto.ts
+++ api/crypto.ts
@@ -1,6 +1,6 @@
 export const fetchCryptoData = async (id: string, days: number): Promise<{time: string;price:number}[]> => {
     const res = await fetch(
-        `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}`
+        `${process.env.NEXT_PUBLIC_API_URL}/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}`
     );
 
     if (!res.ok) throw new Error("Failed to fetch crypto data");
